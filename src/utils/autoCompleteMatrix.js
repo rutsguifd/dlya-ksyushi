968d7c3b8dd109496bdfe8965c4d0a3eaa500834
rutsguifd <74872948+rutsguifd@users.noreply.github.com>
2022-09-29 10:08:37 +0300

    Update autoCompleteMatrix.js

--- src/utils/autoCompleteMatrix.js
+++ src/utils/autoCompleteMatrix.js
@@ -1,8 +1,10 @@
 export const autoCompleteMatrix = (col) => {
+  //створюю 2 вимірний масив
   let res = Array(col)
     .fill(0)
     .map(() => new Array(col).fill(1));
-
+  
+  //заповнюю його по тому як казала глебена (1 1/2 1/3 і тд)
   for (let i = 0; i < col; i++) {
     for (let j = 0; j < col; j++) {
       res[i][j] = 1 / (i + 1 + j + 1 - 1);
@@ -11,7 +13,7 @@ export const autoCompleteMatrix = (col) => {
   return res;
 };
 
-
+//ця залупа не працює думаю там треба через мар або редюс делати
 export const autoCompleteMatrix2 = (col) => {
   let res = []
   let res2 = []
